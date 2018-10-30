37f6b52202757255cec7f6b3c1089b72d22cd3b3
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-30 13:54:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ this.training = training
 //   this.veto = function(){
 //     return 'No, I must disagree'
 //   }
+  BoardMember.prototype.veto = function (){
+    return 'No, I must disagree'
+  };
 //   this.approve = function(){
 //     return 'You can do that!'
 //   }
+  BoardMember.prototype.approve = function (){
+    return 'You can do that!'
+  };
 //   this.doCharity = function(){
 //     return "I like to help people."
 //   }
-//
+  BoardMember.prototype.doCharity = function (){
+    return 'I like to help people.'
+  };
 //   this.releasePressStatement = function(){
 //     return "You will see great things from Scuber."
 //   }
