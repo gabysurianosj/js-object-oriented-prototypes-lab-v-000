cfb3b02398b4b9ae1a4248c3979d9f5a9bc1f3e9
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-30 13:55:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,8 +24,13 @@ this.training = training
 //   this.releasePressStatement = function(){
 //     return "You will see great things from Scuber."
 //   }
-//
+  BoardMember.prototype.releasePressStatement = function (){
+    return 'You will see great things from Scuber.'
+  };
 //   this.sayHi = function(){
 //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 //   }
+  BoardMember.prototype.sayHi = function (){
+    return 'Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.'
+  };
 // }
